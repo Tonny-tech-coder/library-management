4121acde999fe3fac0f9a50ba4ab9f36ec05f69e
manishimwe1 <emmymanishimwem@gmail.com>
2026-04-20 10:51:27 +0200

    Refactor book display structure and styles; add initial book data

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const books =[
+    {
+        title: "Book Title",
+        author: "John Doe",
+        description: "Lorem ipsum dolor sit amet consectetur ",
+        imgSrc: "./image/cover.jpg"
+    },
+    {
+        title: "Book Title",
+        author: "John Doe",
+        description: "Lorem ipsum dolor sit amet consectetur ",
+        imgSrc: "./image/cover.jpg"
+    },
+    {
+        title: "Book Title",
+        author: "John Doe",
+        description: "Lorem ipsum dolor sit amet consectetur ",
+        imgSrc: "./image/cover.jpg"
+    },
+    {
+        title: "Book Title",
+        author: "John Doe",
+        description: "Lorem ipsum dolor sit amet consectetur ",
+        imgSrc: "./image/cover.jpg"
+    },
+    {
+        title: "Book Title",
+        author: "John Doe",
+        description: "Lorem ipsum dolor sit amet consectetur ",
+        imgSrc: "./image/cover.jpg"
+    },
+]
